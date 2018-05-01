f5fcb0d249324fd67df858a1f2d91154c2fda0a0
Geoff Shannon <geoffpshannon@gmail.com>
2018-05-01 03:31:49 -0700

    Add a HandleEffects interceptor and MutateState Effect

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,10 @@ extern crate mopa;
 extern crate futures;
 
 use std::any::TypeId;
+use std::cell::RefCell;
 use std::collections::{HashMap,VecDeque};
 use std::marker::PhantomData;
+use std::rc::Rc;
 
 use futures::{future,Future};
 
@@ -27,6 +29,14 @@ macro_rules! with_any_map {
             pub fn insert<T: 'static + $trait_>(&mut self, value: T) {
                 self.0.insert(TypeId::of::<T>(), Box::new(value));
             }
+
+            pub fn values(&self) -> std::collections::hash_map::Values<TypeId, Box<$trait_>> {
+                self.0.values()
+            }
+
+            pub fn values_mut(&mut self) -> std::collections::hash_map::ValuesMut<TypeId, Box<$trait_>> {
+                self.0.values_mut()
+            }
         }
     }
 }
@@ -39,7 +49,9 @@ pub trait Coeffect: Any {
 mopafy!(Coeffect);
 with_any_map!(Coeffect, CoeffectMap);
 
-pub trait Effect: Any {}
+pub trait Effect: Any {
+    fn action(&mut self);
+}
 mopafy!(Effect);
 with_any_map!(Effect, EffectMap);
 
@@ -106,6 +118,54 @@ where C: Coeffect,
     }
 }
 
+pub struct HandleEffects<E>(PhantomData<E>);
+
+impl<E> HandleEffects<E>
+{
+    pub fn new() -> HandleEffects<E> {
+        HandleEffects(PhantomData)
+    }
+}
+
+impl<E> Interceptor for HandleEffects<E>
+where E: 'static,
+{
+    type Error = E;
+
+    fn before(&self, context: Context<Self::Error>) -> Box<Future<Item = Context<Self::Error>,
+                                                                      Error = Self::Error>> {
+        Box::new(future::ok(context))
+    }
+
+    fn after(&self, mut context: Context<Self::Error>) -> Box<Future<Item = Context<Self::Error>,
+                                                                 Error = Self::Error>> {
+        for e in context.effects.values_mut() {
+            e.action();
+        }
+        Box::new(future::ok(context))
+    }
+}
+
+pub struct MutateState<S, F> {
+    state_ref: Rc<RefCell<S>>,
+    mutate: F,
+}
+
+impl<S, F> MutateState<S, F> {
+    pub fn new(state_ref: Rc<RefCell<S>>, mutate: F) -> MutateState<S, F> {
+        MutateState { state_ref, mutate }
+    }
+}
+
+impl<S, F> Effect for MutateState<S, F>
+where S: 'static,
+      F: 'static + FnMut(&mut S)
+{
+    fn action(&mut self) {
+        (&mut self.mutate)(&mut self.state_ref.borrow_mut())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -132,5 +192,20 @@ mod tests {
         let i= InjectCoeffect::<Test, ()>::new();
         let new_ctx = i.before(context).wait().unwrap();
         assert_eq!(Some(&Test(101)), new_ctx.coeffects.get::<Test>());
+    }
+
+    struct State(u8);
+
+    #[test]
+    fn test_effect_interceptor() {
+        let mut context: Context<()> = Context::new();
+        let i: HandleEffects<()> = HandleEffects::new();
+
+        let state = Rc::new(RefCell::new(State(0)));
+        let e = MutateState::new(Rc::clone(&state), |state: &mut State| state.0 = 10);
+        context.effects.insert(e);
+        i.after(context);
+
+        assert_eq!(state.borrow().0, 10);
     }
 }
